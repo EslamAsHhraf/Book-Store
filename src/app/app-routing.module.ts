ae4bbbb020b7588b989de9dcc7671ecc79fb70d5
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-08-17 21:06:44 +0300

    solve error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { BookPageComponent } from './components/book-page/book-page.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+const routes: Routes = [
+  { path: '', component: BookListComponent },
+  { path: 'search/:searchTerm', component: BookListComponent },
+  { path: 'book/:id', component: BookPageComponent },
+  { path: 'cart-page', component: CartPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { SearchComponent } from './components/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { BookPageComponent } from './components/book-page/book-page.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookListComponent,
+    SearchComponent,
+    BookPageComponent,
+    CartPageComponent,
+    BookDetailsComponent,
+    NotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
